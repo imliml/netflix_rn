0115cd326274f5c9f21a69823006050405905f4f
lsr <imlimr1232@gmail.com>
2020-10-17 13:09:21 +0900

    add tabBar icon

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "react-native-vector-icons";
 import MovieScreen from "./screens/MovieScreen";
 import TVScreen from "./screens/TVScreen";
-import Search from "./screens/Search";
+import SearchScreen from "./screens/SearchScreen";
 
-// export default function App() {
-//   return <MovieScreen />;
-// }
-
-const TabNavigator = createBottomTabNavigator({
-  Movie: {
-    screen: MovieScreen,
+const TabNavigator = createBottomTabNavigator(
+  {
+    Movie: {
+      screen: MovieScreen,
+    },
+    TV: {
+      screen: TVScreen,
+    },
+    Search: {
+      screen: SearchScreen,
+    },
   },
-  TV: {
-    screen: TVScreen,
-  },
-  Search: {
-    screen: Search,
-  },
-});
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Movie") {
+          iconsName = "ios-home";
+        } else if (routeName === "TV") {
+          iconsName = "ios-tv";
+        } else if (routeName === "Search") {
+          iconName = "ios-search";
+        }
+        return (
+          <Ionicons
+            name={iconsName}
+            size={horizontal ? 20 : 25}
+            color={tintColor}
+          />
+        );
+      },
+    }),
+  }
+);
 
 export default createAppContainer(TabNavigator);
